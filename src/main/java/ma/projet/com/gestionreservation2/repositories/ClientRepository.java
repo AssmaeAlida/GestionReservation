35b858a2eedb3b848da174f462b0e1430fff9daa
Assmae Alida <120457808+AssmaeAlida@users.noreply.github.com>
2024-12-13 20:32:38 +0100

    Delete src/main/java/ma/projet/com/gestionreservation2/repositories/ClientRepository.java

--- src/main/java/ma/projet/com/gestionreservation2/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.projet.com.gestionreservation2.repositories;
-
-import ma.projet.com.gestionreservation2.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
