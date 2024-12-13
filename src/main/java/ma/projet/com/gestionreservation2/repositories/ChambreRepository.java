8d395b8b5f9648348a7df5de6455c69e1db639a6
Assmae Alida <120457808+AssmaeAlida@users.noreply.github.com>
2024-12-13 20:32:49 +0100

    Delete src/main/java/ma/projet/com/gestionreservation2/repositories/ChambreRepository.java

--- src/main/java/ma/projet/com/gestionreservation2/repositories/ChambreRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.projet.com.gestionreservation2.repositories;
-
-import ma.projet.com.gestionreservation2.entities.Chambre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChambreRepository extends JpaRepository<Chambre, Long> {
-}
-
